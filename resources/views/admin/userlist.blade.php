96a892bc6613181f48c81c65ec67d50ccbe096bf
Suhairi <suhairi81@gmail.com>
2022-03-30 15:34:57 +0800

    20220330 003

--- resources/views/admin/userlist.blade.php
+++ resources/views/admin/userlist.blade.php
@@ -9,6 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                <table class="table-auto">
+                  <thead>
+                    <tr>
+                      <th>Song</th>
+                      <th>Artist</th>
+                      <th>Year</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                      <td>Malcolm Lockyer</td>
+                      <td>1961</td>
+                    </tr>
+                    <tr>
+                      <td>Witchy Woman</td>
+                      <td>The Eagles</td>
+                      <td>1972</td>
+                    </tr>
+                    <tr>
+                      <td>Shining Star</td>
+                      <td>Earth, Wind, and Fire</td>
+                      <td>1975</td>
+                    </tr>
+                  </tbody>
+                </table>
                     
 
                 </div>
